5acbbd2c71a6e946fa4cb171f87a407d524f525c
Alex Flanagan <alex.flanagan@gmail.com>
2018-04-06 13:15:00 -0400

    Pythonista HTTP API plus driver

---
+++ ConceptMap.py
@@ -0,0 +1,21 @@
+# This is a project to define a topology of meaning, to allow an agent
+# to maintain context by locating itself spatially within the map.
+from bottle import route, run, template
+import requests
+import threading
+
+# Define API routes.
+@route('/hello/<name>')
+def index(name):
+	return template('<b>Hello {{name}}</b>!', name=name)
+
+# Start webserver on a thread (not a process for Pythonista iOS security reasons).
+t = threading.Thread(target=run, kwargs={'host': 'localhost', 'port': 80 })
+t.start()
+
+# Call the API.
+r = requests.request('GET', 'http://localhost/hello/python')
+print(r.content)
+
+# Wait for the webserver thread to terminate.
+t.join()
