ff789769a373506cedaf3ad07542f898db439ac9
Alex Flanagan <alex.flanagan@gmail.com>
2018-04-06 17:18:13 -0400

    Should work cross-platform now.

--- ConceptMap.py
+++ ConceptMap.py
@@ -7,18 +7,31 @@ import threading
 # Built ins.
 import os
 import sys
+import time
 
 # Consts
-port = 8080
 host = 'localhost'
+port = 8080
 
+# If we've passed in a port, use that.
 if len(sys.argv) == 2:
 	port = int(sys.argv[1])
 
+# Override port with environment.
 if 'PORT' in os.environ:
 	port = int(os.environ['PORT'])
+	# TODO: HACK: We're probably on Heroku, so bind to zeros instead of localhost
+	host = '0.0.0.0'
 
-url = 'http://%s:%s/hello/url' % (host, 80)
+# Construct base URL for query.
+url = 'http://%s:%s' % (host, port)
+
+# Override base URL for query if in environment.
+if 'URL' in os.environ:
+	url = os.environ['URL']
+
+# Append path to base URL.
+url = os.path.join('%s','%s') % (url, os.path.join('hello', 'testerington'))
 
 # Define API routes.
 @route('/hello/<name>')
@@ -26,13 +39,16 @@ def index(name):
 	return template('<b>Hello {{name}}</b>!', name=name)
 
 # Start webserver on a thread (not a process for Pythonista iOS security reasons).
-#run(host=host, port=port)
-
-t = threading.Thread(target=run, kwargs={'host': '0.0.0.0', 'port': port })
+#run(host='0.0.0.0', port=port)
+t = threading.Thread(target=run, kwargs={'host': host, 'port': port })
 t.start()
 
+# TODO: HACK: Wait for the thread to start.
 while not t.is_alive():
 	pass
+
+# TODO: HACK: Seriously, wait for the server to start.
+time.sleep(1)
 
 # Call the API.
 r = requests.request('GET', url)
