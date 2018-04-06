f206ac04bf8ea877f75a956d5626e47fbade63cf
Alex Flanagan <alex.flanagan@gmail.com>
2018-04-06 15:09:27 -0400

    Added args and envs

--- ConceptMap.py
+++ ConceptMap.py
@@ -2,22 +2,24 @@
 # to maintain context by locating itself spatially within the map.
 from bottle import route, run, template
 import requests
-
-# Built in packages.
 import threading
-import os
+
+# Built ins.
+import sys
 
 # Define API routes.
 @route('/hello/<name>')
 def index(name):
 	return template('<b>Hello {{name}}</b>!', name=name)
+	
+port = sys.argv[1] or os.environ['PORT']
 
 # Start webserver on a thread (not a process for Pythonista iOS security reasons).
-t = threading.Thread(target=run, kwargs={'host': 'localhost', 'port': os.environ['PORT'] })
+t = threading.Thread(target=run, kwargs={'host': 'localhost', 'port': port })
 t.start()
 
 # Call the API.
-r = requests.request('GET', 'http://localhost/hello/python')
+r = requests.request('GET', 'http://localhost:8080/hello/python')
 print(r.content)
 
 # Wait for the webserver thread to terminate.
